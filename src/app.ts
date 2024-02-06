8b9f890c8148d2106b92fc02a26d1884d7dd0fee
Elihu Nibra <nibraelihu@gmail.com>
2024-02-06 13:06:23 -0600

    Initial commit

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import cors from "cors";
+
+export const app = express();
+
+const corsOptions = {
+    origin: "http://127.0.0.1:5173",
+    optionsSuccessStatus: 200
+};
+
+app.use(cors(corsOptions));
+
+app.get("/", (_req, res) => {
+    res.json({ "test": "Instalacion de dependencias y librerias" });
+});
